ed67702532eefcc29a9eec011e093d7755071c02
Maciej Mucha <mmucha@ioki.com.pl>
2013-03-13 13:53:51 +0100

    init

---
+++ Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mmucha1\TwigExtBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+}
---
+++ Mmucha1TwigExtBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mmucha1\TwigExtBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Mmucha1TwigExtBundle extends Bundle
+{
+}
